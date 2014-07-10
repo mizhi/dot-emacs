9a529b1a9096c713fcd2f5dfb08f43ca5b3168da
Mitchell Peabody <mizhi@cognius.com>
2014-07-10 17:31:43 -0400

    Add marmelade repo

--- init.el
+++ init.el
@@ -23,8 +23,8 @@
 ;; use package management when in emacs >= 24. Is this a good idea? I don't know.
 (when (>= emacs-major-version 24)
   (require 'package)
-  (add-to-list 'package-archives
-	       '("melpa" . "http://melpa.milkbox.net/packages/") t)
+  (add-to-list 'package-archives '("melpa" . "http://melpa.milkbox.net/packages/") t)
+  (add-to-list 'package-archives '("marmelade" . "http://marmalade-repo.org/packages/") t)
 
   (package-initialize)
 
