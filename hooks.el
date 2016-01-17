e77befd46ff9fed0290019e231297ee5bf002685
Mitchell Peabody <mizhi@cognius.com>
2016-01-16 23:44:27 -0500

    Remove desktop-after-read-hook

--- hooks.el
+++ hooks.el
@@ -1,10 +1,3 @@
-(add-hook 'desktop-after-read-hook
-          '(lambda ()
-             ;; desktop-remove clears desktop-dirname
-             (setq desktop-dirname-tmp desktop-dirname)
-             (desktop-remove)
-             (setq desktop-dirname desktop-dirname-tmp)))
-
 (add-hook 'prog-mode-hook
           (lambda ()
             (rvm-autodetect-ruby)
