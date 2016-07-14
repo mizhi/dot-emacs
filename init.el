e622e967da6c2dce5dbc25de9f6ab7be4b724ee9
Mitch Peabody <mpeabody@panoramaed.com>
2016-07-14 08:27:08 -0400

    Spelling fix

--- init.el
+++ init.el
@@ -4,7 +4,7 @@
 ;; You may delete these explanatory comments.
 (require 'package)
 (add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/"))
-(add-to-list 'package-archives '("marmelade" . "https://marmalade-repo.org/packages/"))
+(add-to-list 'package-archives '("marmalade" . "https://marmalade-repo.org/packages/"))
 (package-initialize)
 
 (defun load-init-el (filename)
